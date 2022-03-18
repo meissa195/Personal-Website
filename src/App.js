efb7ca6d7a1644c74130e637c523c7aac6ef3ca1
Mohamed <mgeissa13@gmail.com>
2022-03-18 12:42:11 -0700

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Navbar } from './components/Navbar';
 import Header from './components/Header';
